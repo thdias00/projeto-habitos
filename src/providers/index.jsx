9ccde9da25fcd0830abcf0311cfdbcb5b632d2d0
Thiago Dias <thiago.dias.148116@gmail.com>
2021-12-09 16:30:08 -0300

    initial commit

--- src/providers/index.jsx
+++
@@ -1,6 +0,0 @@
-const Providers = ({ children }) => {
-    return (
-        { children }
-    )
-};
-export default Providers;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,23 +1,8 @@
 import { Switch, Route } from 'react-router-dom';
 
-const Route = () => {
+const Routes = () => {
     return (
-        <Switch>
-            <Route exact path="/">
-                <Login />
-            </Route>
-            <Route path="/signup">
-                <Signup />
-            </Route>
-            <Route path="/dashboard">
-                <Dashboard />
-            </Route>
-            <Route path="/habitsandgroups">
-                <HabitsAndGroups />
-            </Route>
-            <Route path="/groups">
-                <Groups />
-            </Route>
-        </Switch>
+        <div>HappyHabits</div>
     )
 }
+export default Routes;
